9538b592bb94407969132dde4c1c8b08adfa2d74
T <r1102028@ucll.be>
2025-12-15 15:15:36 +0100

    appupdated

--- app.py
+++ app.py
@@ -6,14 +6,13 @@ pygame.init()
 
 def create_main_surface():
     screen_size = (1024, 768)
-    return pygame.display.set_mode(screen_size)
+    pygame.display.set_mode(screen_size)
 
 
-Screen = create_main_surface
+create_main_surface()
 
 while True:
 
     pass
 
-Screen = create_main_surface
 
