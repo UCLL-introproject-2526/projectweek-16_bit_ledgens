d512a417643440cba99af70a2e4eb06493cf23ad
Sander Goethals <sander.goethals@student.ucll.be>
2025-12-15 14:55:07 +0100

    added screen

--- app.py
+++ app.py
@@ -3,3 +3,17 @@ import pygame
 
 # Initialize Pygame
 pygame.init()
+
+def create_main_surface():
+    screen_size = (1024, 768)
+    return pygame.display.set_mode(screen_size)
+
+
+Screen = create_main_surface
+
+while True:
+
+    pass
+
+Screen = create_main_surface
+
